0e54ceb796a86a8468c379c96de1bcce56a3c140
haitam-elgharras <elgharras.haitam@gmail.com>
2024-08-15 21:31:54 +0100

    JUnit's @Suite annotation to group multiple test classes into a single test suite

---
+++ src/test/java/AllTestsSuite.java
@@ -0,0 +1,14 @@
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+@Suite
+@SelectClasses({
+        StringHelperTest.class,
+        StringHelperParameterizedTest.class,
+        ArrayCompareTest.class
+})
+@SuiteDisplayName("JUnit 5 All Tests Suite")
+public class AllTestsSuite {
+    // This class remains empty, it is used only as a holder for the above annotations
+}
