d369ba5db12c9656fb7ad212d22259670f22a9fb
haitam-elgharras <elgharras.haitam@gmail.com>
2024-08-15 23:01:42 +0100

    Init Mockito

---
+++ src/main/java/com/in28minutes/Mockito/business/TodoBusinessImpl.java
@@ -0,0 +1,37 @@
+package com.in28minutes.Mockito.business;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.in28minutes.Mockito.data.api.TodoService;
+
+// SUT or System Under Test
+public class TodoBusinessImpl {
+
+    // Dependency of our SUT
+    private TodoService todoService;
+
+    TodoBusinessImpl(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    public List<String> retrieveTodosRelatedToSpring(String user) {
+        List<String> filteredTodos = new ArrayList<String>();
+        List<String> allTodos = todoService.retrieveTodos(user);
+        for (String todo : allTodos) {
+            if (todo.contains("Spring")) {
+                filteredTodos.add(todo);
+            }
+        }
+        return filteredTodos;
+    }
+
+    public void deleteTodosNotRelatedToSpring(String user) {
+        List<String> allTodos = todoService.retrieveTodos(user);
+        for (String todo : allTodos) {
+            if (!todo.contains("Spring")) {
+                todoService.deleteTodo(todo);
+            }
+        }
+    }
+}
